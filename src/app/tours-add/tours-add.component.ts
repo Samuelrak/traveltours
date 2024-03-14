1117207680d68c03bc9611e5c31311c5af6cec90
Lucia Bodnarova <Lucia.Bodnarova@telekom.external.com>
2024-03-14 13:40:59 +0100

    tour-edit form validator for name and location created

--- src/app/tours-add/tours-add.component.ts
+++ src/app/tours-add/tours-add.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ToursService } from '../services/tours.service';
 import { MessageService } from '../services/message.service';
 
